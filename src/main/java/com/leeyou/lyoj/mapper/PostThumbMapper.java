af55fb70a0cfe6209f951b60d24cb3014ee8febb
iamnotloser <1689986307@qq.com>
2025-01-12 09:09:37 +0800

    first init

---
+++ src/main/java/com/leeyou/lyoj/mapper/PostThumbMapper.java
@@ -0,0 +1,18 @@
+package com.leeyou.lyoj.mapper;
+
+import com.leeyou.lyoj.model.entity.PostThumb;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * 帖子点赞数据库操作
+ *
+ * @author <a href="https://github.com/liyupi">程序员鱼皮</a>
+ * @from <a href="https://yupi.icu">编程导航知识星球</a>
+ */
+public interface PostThumbMapper extends BaseMapper<PostThumb> {
+
+}
+
+
+
+
